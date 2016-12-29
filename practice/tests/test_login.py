9a92c0e7d5e6add6a2644333cfed179e5757c8bb
namartin <NMartin11@winona.edu>
2016-12-29 17:33:11 -0600

    wip

--- practice/tests/test_login.py
+++ practice/tests/test_login.py
@@ -1,15 +1,22 @@
 import pytest
+from practice.support import HerokuappPage
 from selenium import webdriver
 
 
 def test_login_succeeded(driver):
     driver.get("http://the-internet.herokuapp.com/login")
-    driver.find_element_by_id('username').send_keys('tomsmith')
-    driver.find_element_by_id('password').send_keys('SuperSecretPassword!')
+    page = HerokuappPage(driver)
+    page.username_field = 'tomsmith'
+    page.password_field = 'SuperSecretPassword!'
+    page.submit_button.click()
 
-    submit_button = driver.find_element_by_css_selector("button[type='submit']")
-    submit_button.click()
-
+    # driver.find_element_by_id('username').send_keys('tomsmith')
+    # driver.find_element_by_id('password').send_keys('SuperSecretPassword!')
+    #
+    # submit_button = driver.find_element_by_css_selector("button[type='submit']")
+    # submit_button.click()
     assert driver.find_element_by_css_selector("div.flash.success").is_displayed()
 
+if __name__ == "__main__":
+    pytest.main()
 
